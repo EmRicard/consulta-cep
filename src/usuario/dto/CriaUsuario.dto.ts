02dbf3975930022e8d211a30d751842e23c05742
Ricardo Cedraz <emanuel.cedrazta@hotmail.com>
2023-12-05 12:11:41 -0300

    Email Validator Complete

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -1,4 +1,5 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { EmailEhUnico } from "../validacao/email-eh-unico.validator";
 
 
 export class CriaUsuarioDTO{
@@ -7,6 +8,7 @@ export class CriaUsuarioDTO{
     nome: string;
 
     @IsEmail()
+    @EmailEhUnico({message: 'e-mail em uso'})
     email: string;
 
     @MinLength(6)
--- src/usuario/validacao/email-eh-unico.validator.ts
+++ src/usuario/validacao/email-eh-unico.validator.ts
@@ -1,4 +1,4 @@
-import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+import { ValidateByOptions, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, registerDecorator } from "class-validator";
 import { UsuarioRepository } from "../usuario.repository";
 import { Injectable } from "@nestjs/common";
 
@@ -6,13 +6,23 @@ import { Injectable } from "@nestjs/common";
 @ValidatorConstraint({ async: true })
 export class EmailEhUnicoValidator implements ValidatorConstraintInterface{
 
-    constructor(private UsuarioRepository: UsuarioRepository){
-
-    }
+    constructor(private UsuarioRepository: UsuarioRepository){}
 
     async validate(value: any, validationArguments?: ValidationArguments | undefined): Promise<boolean> {
         const usuarioComEmailExiste = await this.UsuarioRepository.existeComEmail(value);
         return !usuarioComEmailExiste;
     }
     
+}
+
+export const EmailEhUnico = (opcoesDeValidacao: ValidationOptions) => {
+    return (objeto: Object, prorpiedade: string) => {
+        registerDecorator({
+            target: objeto.constructor,
+            propertyName: prorpiedade,
+            options: opcoesDeValidacao,
+            constraints: [],
+            validator: EmailEhUnicoValidator
+        })
+    }
 }
